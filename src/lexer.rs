eb94caa1e9c705eeea894d60808b0423ae161631
Matthew Wright <wrightm@gmail.com>
2014-04-24 00:00:14 -0700

    Make the lexer into an iterator

--- src/lexer.rs
+++ src/lexer.rs
@@ -49,64 +49,85 @@ pub enum Token {
     String,
 }
 
-pub struct Lexer {
+pub struct Lexer<T> {
     rules: ~[LexerRule],
+    current_line: Option<~str>,
+    pos: Option<uint>,
+    line_iter: ~T,
 }
 
-impl Lexer {
-    pub fn new() -> Lexer {
-        macro_rules! lexer {
+
+
+impl<T: Iterator<~str>> Lexer<T> {
+    pub fn new<T>(line_iter: ~T) -> Lexer<T> {
+        macro_rules! lexer_rules {
             ( $( $t:expr => $r:expr ),*) => (
-                Lexer { rules: ~[ $( LexerRule { matcher: regexp!(concat!("^(?:", $r, ")")), token: $t } ),* ] }
+                ~[ $( LexerRule { matcher: regexp!(concat!("^(?:", $r, ")")),
+                                  token: $t } ),* ]
             )
         }
 
         // Note: rules are in decreasing order of priority if there's a
         // conflict. In particular, reserved words must go before Ident.
-        lexer! {
-            // Whitespace
-            WS         => r"\s|//.*|(?s)/\*.*\*/",
+        Lexer {
+            current_line: None,
+            pos: None,
+            line_iter: line_iter,
+            rules:
+            lexer_rules! {
+                // Whitespace
+                WS         => r"\s|//.*|(?s)/\*.*\*/",
 
-            // Reserved words
-            Let        => r"let",
+                // Reserved words
+                Let        => r"let",
 
-            // Symbols
-            LParen     => r"\(",
-            RParen     => r"\)",
-            LBrace     => r"\{",
-            RBrace     => r"\}",
-            LBracket   => r"\[",
-            RBracket   => r"\]",
-            Less       => r"<",
-            Greater    => r">",
-            And        => r"&",
-            Or         => r"\|",
-            Xor        => r"\^",
-            AndAnd     => r"&&",
-            OrOr       => r"\|\|",
-            Add        => r"\+",
-            Sub        => r"-",
-            Mul        => r"\*",
-            Div        => r"/",
-            Lsh        => r"<<",
-            Rsh        => r">>",
-            Colon      => r":",
-            Semi       => r";",
-            Eq         => r"=",
-            Bang       => r"!",
+                // Symbols
+                LParen     => r"\(",
+                RParen     => r"\)",
+                LBrace     => r"\{",
+                RBrace     => r"\}",
+                LBracket   => r"\[",
+                RBracket   => r"\]",
+                Less       => r"<",
+                Greater    => r">",
+                And        => r"&",
+                Or         => r"\|",
+                Xor        => r"\^",
+                AndAnd     => r"&&",
+                OrOr       => r"\|\|",
+                Add        => r"\+",
+                Sub        => r"-",
+                Mul        => r"\*",
+                Div        => r"/",
+                Lsh        => r"<<",
+                Rsh        => r">>",
+                Colon      => r":",
+                Semi       => r";",
+                Eq         => r"=",
+                Bang       => r"!",
 
-            // Literals
-            Ident      => r"[a-zA-Z_]\w*",
-            Number     => r"\d",
-            HexNumber  => r"0[xX][:xdigit:]+",
-            String     => r#""(?:\\"|[^"])*""#
+                // Literals
+                Ident      => r"[a-zA-Z_]\w*",
+                Number     => r"\d",
+                HexNumber  => r"0[xX][:xdigit:]+",
+                String     => r#""(?:\\"|[^"])*""#
+            }
         }
     }
+}
 
-    pub fn tokenize(&self, s: &str) -> (~[(Token, ~str)]) {
-        let mut pos = 0u;
-        let mut result = vec!();
-        while pos < s.len() {
+impl<T: Iterator<~str>> Iterator<(Token, ~str)> for Lexer<T> {
+    fn next(&mut self) -> Option<(Token, ~str)> {
+        loop {
+            if self.pos.is_none() {
+                self.current_line = self.line_iter.next();
+                if self.current_line.is_none() { return None; }
+                self.pos = Some(0);
+            }
+
+            let mut pos = self.pos.unwrap();
+            let s = self.current_line.clone().unwrap();
+
             let mut longest = 0u;
             let mut best = None;
             for rule in self.rules.iter() {
@@ -123,19 +144,25 @@ impl Lexer {
                 }
             }
             pos += longest;
+            if pos == s.len() {
+                self.pos = None;
+                self.current_line = None;
+            } else {
+                self.pos = Some(pos);
+            }
+
             match best.unwrap() {
-                (WS, _) => {},
-                (t, s) => result.push((t, s.to_owned()))
+                (WS, _) => continue,
+                (t, s) => return Some((t, s.to_owned()))
             }
         }
-
-        result.as_slice().to_owned()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::iter::Repeat;
 
     fn compare(actual: &[(Token, ~str)], expected: &[(Token, ~str)]) {
         for (&(actual_token, ref actual_str),
@@ -151,8 +178,23 @@ mod tests {
 
     #[test]
     fn test() {
-        let l = Lexer::new();
+        let test_iter = Repeat::new(~"f(x)+1");
+        let mut l = Lexer::<Repeat<~str>>::new::<Repeat<~str>>(~test_iter);
 
+        print!("{:?}\n", l.next());
+        print!("{:?}\n", l.next());
+        print!("{:?}\n", l.next());
+        print!("{:?}\n", l.next());
+        print!("{:?}\n", l.next());
+        print!("{:?}\n", l.next());
+        print!("{:?}\n", l.next());
+        print!("{:?}\n", l.next());
+        print!("{:?}\n", l.next());
+        print!("{:?}\n", l.next());
+        print!("{:?}\n", l.next());
+
+        fail!();
+        /*
         compare(l.tokenize(
             r#"f(x - /* I am a comment */ 0x3f5B)+1 "Hello\" World")"#),
                 [(Ident, ~"f"),
@@ -172,6 +214,7 @@ mod tests {
                  (Ident, ~"int"),
                  (Eq, ~"="),
                  (Number, ~"5"),
-                 ]);
+                 ]);*/
+    
     }
 }
