3df0ece797bd6c9996d85eade16b186b316c1a6a
Kevin Murphy <kemurphy.cmu@gmail.com>
2014-04-27 06:18:21 -0700

    Add a bunch of traits to Lexer to make LexerRules more powerful so we can handle tokens with more context

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,13 +2,6 @@ use regex::Regex;
 use std::iter;
 use std::slice::CloneableVector;
 
-/// A single regex for a token.
-#[deriving(Clone)]
-struct LexerRule {
-    matcher: Regex,
-    token: Token,
-}
-
 #[deriving(Eq, Clone, Show)]
 pub enum Token {
     // Whitespace
@@ -21,6 +14,10 @@ pub enum Token {
     Else,
     Fn,
     Return,
+    True,
+    False,
+    U32,
+    I32,
 
     // Symbols
     LParen,
@@ -56,16 +53,9 @@ pub enum Token {
     Period,
 
     // Literals
-    Ident,
-    Number,
-    HexNumber,
-    String,
-    True,
-    False,
-
-    // Basic types
-    U32,
-    I32,
+    Ident(~str),
+    Number(u64),
+    String(~str),
 
     // Special
     Eof,
@@ -74,7 +64,6 @@ pub enum Token {
 #[deriving(Show, Eq)]
 pub struct SourceToken {
     pub tok: Token,
-    pub txt: ~str
 }
 
 struct LineContext {
@@ -82,79 +71,141 @@ struct LineContext {
     line: ~str,
 }
 
+struct LexerRule<T, U> {
+    matcher: T,
+    maker: U,
+}
+
+// Trait to make lexer_rules! forget the type parameters of LexerRule
+trait LexerRuleT {
+    fn run(&self, s: &str) -> Option<(uint, Token)>;
+}
+
+// The idea behind this is to allow very flexible lexer rules in the lexer_rules!
+// macro.  A LexerRule has a rule with which to check for a match, and a factory
+// to produce a Token if the rule's match succeeds (some tokens like Number, String,
+// and Ident carry additional context about what matched).  This way, we can specify
+// a raw string as a rule to do a simple string-prefix match, a Regex to check
+// for a regex match, or optionally more complicated rules to e.g. use capture
+// groups from a Regex and construct a token from those.
+impl<A, T: RuleMatcher<A>, U: TokenMaker<A>> LexerRuleT for LexerRule<T, U> {
+    fn run(&self, s: &str) -> Option<(uint, Token)> {
+        match self.matcher.find(s) {
+            Some((len, args)) => Some((len, self.maker.mk_tok(args))),
+            _ => None
+        }
+    }
+}
+
 pub struct Lexer<T> {
     iter: T,
     linectx: Option<LineContext>,
-    rules: Vec<LexerRule>,
+    rules: Vec<~LexerRuleT>,
 }
 
 impl<T: Iterator<~str>> Lexer<T> {
     pub fn new(line_iter: T) -> Lexer<T> {
+        macro_rules! matcher { ( $e:expr ) => ( regex!(concat!("^(?:", $e, ")"))) }
         macro_rules! lexer_rules {
-            ( $( $t:expr => $r:expr ),*) => (
-                    vec!( $( LexerRule { matcher: regex!(concat!("^(?:", $r, ")")),
-                                         token: $t } ),* )
+            ( $( $c:expr => $m:expr ),*) => (
+                vec!( $( ~LexerRule { matcher: $m, maker: $c } as ~LexerRuleT ),* )
             )
+        }
+
+        // Rule to match a numeric literal and parse it into a number
+        struct NumberRule;
+        impl RuleMatcher<u64> for NumberRule {
+            fn find(&self, s: &str) -> Option<(uint, u64)> {
+                use std::num::from_str_radix;
+
+                let matcher = matcher!(r"(?:0[xX]([:xdigit:]+))|(\d+)");
+                match matcher.captures(s) {
+                    Some(groups) => {
+                        let t = groups.at(0);
+                        let (num_str, radix) = match groups.pos(1) {
+                            Some(_) => (groups.at(1), 16),
+                            None    => (t, 10)
+                        };
+
+                        Some((t.len(), from_str_radix(num_str, radix).take_unwrap()))
+                    },
+                    _ => None
+                }
+            }
+        }
+
+        // Rule to match a string literal and strip off the surrounding quotes
+        struct StringRule;
+        impl RuleMatcher<~str> for StringRule {
+            fn find(&self, s: &str) -> Option<(uint, ~str)> {
+                let matcher = matcher!(r#""((?:\\"|[^"])*)""#);
+                match matcher.captures(s) {
+                    Some(groups) => {
+                        let t = groups.at(0);
+                        Some((t.len(), groups.at(1).to_owned()))
+                    },
+                    _ => None
+                }
+            }
         }
 
         // Note: rules are in decreasing order of priority if there's a
         // conflict. In particular, reserved words must go before Ident.
         let rules = lexer_rules! {
             // Whitespace
-            WS         => r"\s|//.*|(?s)/\*.*\*/",
+            WS         => matcher!(r"\s|//.*|(?s)/\*.*\*/"),
 
             // Reserved words
-            Let        => r"let",
-            True       => r"true",
-            False      => r"false",
-            As         => r"as",
-            If         => r"if",
-            Else       => r"else",
-            Fn         => r"fn",
-            Return     => r"return",
+            Let        => "let",
+            As         => "as",
+            If         => "if",
+            Else       => "else",
+            Fn         => "fn",
+            Return     => "return",
+            True       => "true",
+            False      => "false",
 
             // Basic types; TODO: add more.
-            I32        => r"[iI]32",
-            U32        => r"[uU]32",
+            I32        => matcher!(r"[iI]32"),
+            U32        => matcher!(r"[uU]32"),
 
             // Symbols
-            LParen       => r"\(",
-            RParen       => r"\)",
-            LBrace       => r"\{",
-            RBrace       => r"\}",
-            LBracket     => r"\[",
-            RBracket     => r"\]",
-            Less         => r"<",
-            Greater      => r">",
-            LessEq       => r"<=",
-            GreaterEq    => r">=",
-            Ampersand    => r"&",
-            Pipe         => r"\|",
-            Xor          => r"\^",
-            AmpAmp       => r"&&",
-            PipePipe     => r"\|\|",
-            Plus         => r"\+",
-            Dash         => r"-",
-            Star         => r"\*",
-            ForwardSlash => r"/",
-            Lsh          => r"<<",
-            Rsh          => r">>",
-            Colon        => r":",
-            ColonColon   => r"::",
-            Semicolon    => r";",
-            Eq           => r"=",
-            EqEq         => r"==",
-            Bang         => r"!",
-            Arrow        => r"->",
-            Comma        => r",",
-            QuestionMark => r"\?",
-            Period       => r"\.",
+            LParen       => "(",
+            RParen       => ")",
+            LBrace       => "{",
+            RBrace       => "}",
+            LBracket     => "[",
+            RBracket     => "]",
+            Less         => "<",
+            Greater      => ">",
+            LessEq       => "<=",
+            GreaterEq    => ">=",
+            Ampersand    => "&",
+            Pipe         => "|",
+            Xor          => "^",
+            AmpAmp       => "&&",
+            PipePipe     => "||",
+            Plus         => "+",
+            Dash         => "-",
+            Star         => "*",
+            ForwardSlash => "/",
+            Lsh          => "<<",
+            Rsh          => ">>",
+            Colon        => ":",
+            ColonColon   => "::",
+            Semicolon    => ";",
+            Eq           => "=",
+            EqEq         => "==",
+            Bang         => "!",
+            Arrow        => "->",
+            Comma        => ",",
+            QuestionMark => "?",
+            Period       => ".",
 
             // Literals
-            Ident      => r"[a-zA-Z_]\w*",
-            Number     => r"\d+",
-            HexNumber  => r"0[xX][:xdigit:]+",
-            String     => r#""(?:\\"|[^"])*""#
+            Ident      => matcher!(r"[a-zA-Z_]\w*"),
+            Number     => NumberRule,
+            String     => StringRule
         };
 
         Lexer {
@@ -165,6 +216,7 @@ impl<T: Iterator<~str>> Lexer<T> {
     }
 }
 
+// The meat of the lexer (read this as a stateful flat-map)
 impl<T: Iterator<~str>> Iterator<SourceToken> for Lexer<T> {
     fn next(&mut self) -> Option<SourceToken> {
         loop {
@@ -173,13 +225,12 @@ impl<T: Iterator<~str>> Iterator<SourceToken> for Lexer<T> {
                     let mut longest = 0u;
                     let mut best = None;
                     for rule in self.rules.iter() {
-                        let m = rule.matcher.find(lc.line.slice_from(lc.pos));
+                        let m = rule.run(lc.line.slice_from(lc.pos));
                         match m {
-                            Some((begin, end)) if begin == 0 => {
-                                let s = lc.line.slice(lc.pos, lc.pos + end);
-                                if s.len() > longest {
-                                    best = Some((rule.token, s));
-                                    longest = s.len();
+                            Some((len, tok)) => {
+                                if len > longest {
+                                    best = Some(tok);
+                                    longest = len;
                                 }
                             },
                             _ => {},
@@ -190,13 +241,12 @@ impl<T: Iterator<~str>> Iterator<SourceToken> for Lexer<T> {
 
                     match best {
                         None => fail!("Unexpected input"),
-                        Some((t, s)) if t != WS => {
+                        Some(WS) => {}
+                        Some(tok) => {
                             return Some(SourceToken {
-                                tok: t,
-                                txt: s.to_owned()
+                                tok: tok,
                             })
                         }
-                        _ => {}
                     }
                 }
             }
@@ -214,20 +264,81 @@ impl<T: Iterator<~str>> Iterator<SourceToken> for Lexer<T> {
     }
 }
 
+// A TokenMaker accepts an argument tuple and hands back a token.
+trait TokenMaker<T>{
+    fn mk_tok(&self, args: T) -> Token;
+}
+
+// A raw Token never needs any more arguments, so accept unit and hand back itself.
+impl TokenMaker<()> for Token {
+    fn mk_tok(&self, _: ()) -> Token { self.clone() }
+}
+
+// A Token constructor (Number, String, Ident, etc.) requires arguments for the
+// constructor to make the token, so accept a tuple of those arguments and hand back
+// the constructed token.
+impl<T> TokenMaker<T> for fn(T) -> Token {
+    fn mk_tok(&self, arg: T) -> Token{ (*self)(arg) }
+}
+
+// A RuleMatcher accepts a string slice and tests the encapsulated rule on it.
+// If there is a match it can optionally hand back a string slice corresponding
+// to that match. (The "optionally" part is determined by whether or not the
+// TokenMaker for this RuleMatcher requires the match as an argument, like Ident;
+// the type magic is handled by MaybeArg).
+trait RuleMatcher<T> {
+    fn find<'a>(&self, s: &'a str) -> Option<(uint, T)>;
+}
+
+// Simple string-prefix match
+impl<'a, T: MaybeArg> RuleMatcher<T> for &'a str {
+    fn find<'a>(&self, s: &'a str) -> Option<(uint, T)> {
+        match s.starts_with(*self) {
+            true => Some((self.len(), MaybeArg::maybe_arg(*self))),
+            _ => None
+        }
+    }
+}
+
+// Regex match
+impl<T: MaybeArg> RuleMatcher<T> for Regex {
+    fn find<'a>(&self, s: &'a str) -> Option<(uint, T)> {
+        match self.find(s) {
+            Some((_, end)) => {
+                let t = s.slice(0, end);
+                Some((t.len(), MaybeArg::maybe_arg(t)))
+            },
+            _ => None
+        }
+    }
+}
+
+// Utility trait to optionally grab the match as an argument
+// (useful to avoid unnecessary string copies when we will just throw the result away anyway)
+trait MaybeArg {
+    fn maybe_arg<'a>(arg: &'a str) -> Self;
+}
+
+impl MaybeArg for () {
+    fn maybe_arg<'a>(_: &'a str) { }
+}
+
+impl MaybeArg for ~str {
+    fn maybe_arg<'a>(s: &'a str) -> ~str { s.to_owned() }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::iter::Repeat;
     use std::vec::Vec;
 
-    fn compare(actual: &[SourceToken], expected: &[(Token, ~str)]) {
-        for (ref actual_st, ref expected_tup)
+    fn compare(actual: &[SourceToken], expected: &[Token]) {
+        for (actual_st, expected_tok)
             in actual.iter().zip(expected.iter()) {
-            assert!(actual_st.tok == *expected_tup.ref0() &&
-                    actual_st.txt == *expected_tup.ref1(),
-                    format!("Failure:\n  Tokens:{:?}, {:?}\n  Strings:{:s}, {:s}\n",
-                            actual_st.tok, *expected_tup.ref0(),
-                            actual_st.txt, *expected_tup.ref1()));
+            assert!(actual_st.tok == *expected_tok,
+                    format!("Failure:\n  found {:?}, expected {:?}\n",
+                            actual_st.tok, *expected_tok));
         }
     }
 
@@ -237,26 +348,26 @@ mod tests {
         let tokens1: ~[SourceToken] = FromIterator::from_iter(lexer1);
 
         compare(tokens1,
-                [(Ident, ~"f"),
-                  (LParen, ~"("),
-                  (Ident, ~"x"),
-                  (Dash, ~"-"),
-                  (HexNumber, ~"0x3f5B"),
-                  (RParen, ~")"),
-                  (Plus, ~"+"),
-                  (Number, ~"1"),
-                  (String, ~r#""Hello\" World""#),
+                [Ident(~"f"),
+                  LParen,
+                  Ident(~"x"),
+                  Dash,
+                  Number(0x3f5B),
+                  RParen,
+                  Plus,
+                  Number(1),
+                  String(~r#"Hello\" World"#),
                 ]);
 
         let lexer2 = Lexer::new(vec!(~"let x: int = 5;").move_iter());
         let tokens2: ~[SourceToken] = FromIterator::from_iter(lexer2);
         compare(tokens2,
-                [(Let, ~"let"),
-                 (Ident, ~"x"),
-                 (Colon, ~":"),
-                 (Ident, ~"int"),
-                 (Eq, ~"="),
-                 (Number, ~"5"),
+                [Let,
+                 Ident(~"x"),
+                 Colon,
+                 Ident(~"int"),
+                 Eq,
+                 Number(5),
                  ]);
     }
 }
