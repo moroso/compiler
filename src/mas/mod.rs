6e8d0860bbf328dfa6a98f5cb54918c9b40718c5
Matthew Wright <wrightm@gmail.com>
2015-07-28 00:34:58 -0700

    Support file names on the commandline of the assembler. Closes #44.

--- src/mas/mod.rs
+++ src/mas/mod.rs
@@ -1,14 +1,15 @@
-use std::process;
-use std::io;
-use std::env;
+use std::{process, io, env};
+use std::path::Path;
+use std::fs::File;
 
+use util::lexer::Lexer;
 use self::lexer::{Token, new_asm_lexer};
 use self::parser::AsmParser;
 use self::encoder::encode;
 
 use getopts;
 use getopts::{getopts, reqopt, optopt, optflag};
-use std::io::{Write, BufReader};
+use std::io::{Read, Write, BufReader};
 
 
 pub mod lexer;
@@ -19,7 +20,7 @@ pub mod util;
 pub mod labels;
 pub mod scheduler;
 
-fn print_bin<T: Write>(n: u32, stream: &mut T) {
+fn print_bin(n: u32, stream: &mut Write) {
     // Write in little-endian format.
     (stream.write_all(vec!(
         (n >>  0) as u8,
@@ -36,6 +37,7 @@ pub fn main() {
 
     let opts = [
         optopt("", "fmt", "Set the output format.", "[c|bin|internal]"),
+        optopt("o", "output", "Output file", "<filename>"),
         optflag("h", "help", "Show this help message."),
     ];
 
@@ -69,26 +71,50 @@ pub fn main() {
         print!("Moroso assembler.\n");
     }
 
-    let lexer = new_asm_lexer("<stdin>", BufReader::new(io::stdin()));
-    let peekable = lexer.peekable();
-    let mut parser = AsmParser::new(peekable);
-    let (mut insts, labels) = parser.parse_toplevel();
-    let mut stdout = io::stdout();
+    let mut f = match matches.opt_str("output") {
+        None => Box::new(io::stdout()) as Box<Write>,
+        Some(name) => {
+            let path = Path::new(&name);
+            let file = File::create(&path).unwrap_or_else(|e| panic!("{}", e));
+            Box::new(file) as Box<Write>
+        }
+    };
+
+    let name = if matches.free.len() == 0 {
+        "-"
+    } else if matches.free.len() == 1 {
+        &matches.free[0][..]
+    } else {
+        bail(Some("too many arguments"));
+        panic!()
+    };
+
+    let (mut insts, labels) = if name == "-" {
+        let peekable = new_asm_lexer("<stdin>", BufReader::new(io::stdin())).peekable();
+        let mut parser = AsmParser::new(peekable);
+        parser.parse_toplevel()
+    } else {
+        let path = Path::new(name);
+        let file = File::open(&path).unwrap_or_else(|e| panic!("{}", e));
+        let peekable = new_asm_lexer(name, BufReader::new(file)).peekable();
+        let mut parser = AsmParser::new(peekable);
+        parser.parse_toplevel()
+    };
 
     labels::resolve_labels(&mut insts, &labels);
 
     for packet in insts.iter() {
         match &format_arg[..] {
-            "internal" => print!("{:?}\n", packet),
-            "c" => print!("0x{:08x}, 0x{:08x}, 0x{:08x}, 0x{:08x},\n",
-                          encode(&packet[0]),
-                          encode(&packet[1]),
-                          encode(&packet[2]),
-                          encode(&packet[3])),
-            "bin" => { print_bin(encode(&packet[0]), &mut stdout);
-                       print_bin(encode(&packet[1]), &mut stdout);
-                       print_bin(encode(&packet[2]), &mut stdout);
-                       print_bin(encode(&packet[3]), &mut stdout);
+            "internal" => { write!(&mut *f, "{:?}\n", packet).ok(); },
+            "c" => { write!(&mut *f, "0x{:08x}, 0x{:08x}, 0x{:08x}, 0x{:08x},\n",
+                            encode(&packet[0]),
+                            encode(&packet[1]),
+                            encode(&packet[2]),
+                            encode(&packet[3])).ok(); },
+            "bin" => { print_bin(encode(&packet[0]), &mut *f);
+                       print_bin(encode(&packet[1]), &mut *f);
+                       print_bin(encode(&packet[2]), &mut *f);
+                       print_bin(encode(&packet[3]), &mut *f);
             }
             _ => panic!()
         }
