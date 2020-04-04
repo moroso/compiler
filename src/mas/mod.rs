18ad71ef83a56d5494e14b3697b655e8dabb6709
Matthew Wright <wrightm@gmail.com>
2020-04-03 23:11:30 -0700

    Add "hex" and "bootrom" output options to mas

--- src/mas/mod.rs
+++ src/mas/mod.rs
@@ -32,6 +32,10 @@ fn print_bin(n: u32, stream: &mut dyn Write) {
         ).as_ref())).ok();
 }
 
+fn print_hex(n: u32, stream: &mut dyn Write) {
+    write!(stream, "{:08x}", n).ok();
+}
+
 pub fn main() {
     // TODO: option parsing.
     let args: Vec<String> = env::args().collect();
@@ -39,8 +43,8 @@ pub fn main() {
 
     let mut argopts = Getopts::new();
 
-    argopts.optopt("f", "fmt", "Set the output format.", "[c|bin|internal]");
-    argopts.optopt("", "format", "Alias for fmt", "[c|bin|internal]");
+    argopts.optopt("f", "fmt", "Set the output format.", "[c|bin|hex|bootrom|internal]");
+    argopts.optopt("", "format", "Alias for fmt", "[c|bin|hex|bootrom|internal]");
     argopts.optopt("o", "output", "Output file", "<filename>");
     argopts.optflag("h", "help", "Show this help message.");
 
@@ -117,20 +121,52 @@ pub fn main() {
 
     labels::resolve_labels(&mut insts, &new_labels, 0);
 
-    for packet in &insts {
-        match &format_arg[..] {
-            "internal" => { write!(&mut *f, "{:?}\n", packet).ok(); },
-            "c" => { write!(&mut *f, "0x{:08x}, 0x{:08x}, 0x{:08x}, 0x{:08x},\n",
-                            encode(&packet[0]),
-                            encode(&packet[1]),
-                            encode(&packet[2]),
-                            encode(&packet[3])).ok(); },
-            "bin" => { print_bin(encode(&packet[0]), &mut *f);
-                       print_bin(encode(&packet[1]), &mut *f);
-                       print_bin(encode(&packet[2]), &mut *f);
-                       print_bin(encode(&packet[3]), &mut *f);
+    match &format_arg[..] {
+        "internal" => for packet in &insts {
+            write!(&mut *f, "{:?}\n", packet).ok();
+        },
+        "c" => for packet in &insts {
+            write!(&mut *f, "0x{:08x}, 0x{:08x}, 0x{:08x}, 0x{:08x},\n",
+                   encode(&packet[0]),
+                   encode(&packet[1]),
+                   encode(&packet[2]),
+                   encode(&packet[3])).ok();
+        },
+        "bin" => for packet in &insts {
+            print_bin(encode(&packet[0]), &mut f);
+            print_bin(encode(&packet[1]), &mut f);
+            print_bin(encode(&packet[2]), &mut f);
+            print_bin(encode(&packet[3]), &mut f);
+        },
+        "hex" => for packet in &insts {
+            print_hex(encode(&packet[0]), &mut f); writeln!(f, "").ok();
+            print_hex(encode(&packet[1]), &mut f); writeln!(f, "").ok();
+            print_hex(encode(&packet[2]), &mut f); writeln!(f, "").ok();
+            print_hex(encode(&packet[3]), &mut f); writeln!(f, "").ok();
+        },
+        // Suitable for generating bootrom.hex
+        "bootrom" => {
+            use mas::ast::{InstPacket, InstNode::NopInst};
+
+            fn print_packet(f: &mut dyn Write, packet: &InstPacket) {
+                print_hex(encode(&packet[3]), f);
+                print_hex(encode(&packet[2]), f);
+                print_hex(encode(&packet[1]), f);
+                print_hex(encode(&packet[0]), f);
             }
-            _ => panic!()
-        }
+
+            let it = insts.chunks_exact(2);
+            let r = it.remainder();
+            for s in it {
+                print_packet(&mut f, &s[1]);
+                print_packet(&mut f, &s[0]);
+                writeln!(f, "").ok();
+            }
+            if let &[r] = &r {
+                print_packet(&mut f, &[NopInst, NopInst, NopInst, NopInst]);
+                print_packet(&mut f, &r);
+            }
+        },
+        _ => panic!("Unknown format {}", format_arg),
     }
 }
